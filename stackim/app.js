1d6af5f150b619633cb62f5e2b73771d8ef8f722
Michael Dippery <michael@monkey-robot.com>
2013-02-25 10:47:55 -0500

    Specify port with environment variable $PORT
    This will make it easier to run the app on Heroku, as eventually
    planned.

--- stackim/app.js
+++ stackim/app.js
@@ -1,7 +1,10 @@
 var http = require('http')
 
+var port = process.env.PORT || 8000
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('Hello, world!\n')
-}).listen(8000, '127.0.0.1')
-console.log('Server running at http://127.0.0.1:1337/')
+}).listen(port, function () {
+    console.log('Server running at http://127.0.0.1:' + port + '/')
+})
