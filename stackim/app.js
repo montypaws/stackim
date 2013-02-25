0e2829117e48c028b9e5ceda732aec4442591204
Michael Dippery <michael@monkey-robot.com>
2013-02-25 11:08:30 -0500

    Create basic URL handlers

--- stackim/app.js
+++ stackim/app.js
@@ -2,10 +2,26 @@ var express = require('express')
 
 var app = express();
 
+app.enable('trust proxy')
+
+
 app.get('/', function (req, resp) {
     resp.setHeader('Content-Type', 'text/plain')
     resp.end('Hello, world!\n')
 })
+
+app.get('/:tag', function (req, resp) {
+    var tag = req.params.tag
+    resp.setHeader('Content-Type', 'text/plain')
+    resp.end('-> http://stackoverflow.com/users/' + tag + '\n')
+})
+
+app.put('/:tag', function (req, resp) {
+    var tag = req.params.tag
+    resp.setHeader('Content-Tyoe', 'text/plain')
+    resp.end("Creating tag for '" + tag + "'\n")
+})
+
 
 var port = process.env.PORT || 8000
 app.listen(port, function () {
