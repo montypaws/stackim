1477ac23d06d24aa95e560c48eb090e1ce9052ab
Michael Dippery <michael@monkey-robot.com>
2013-02-25 11:42:23 -0500

    Basic app handling
    The logic is there to create the redirects. Error handling could
    undoubtedly be better and I'm sure my JavaScript/node.js style is
    atrocious, but hey, I'm still learning.

--- stackim/app.js
+++ stackim/app.js
@@ -1,8 +1,15 @@
-var express = require('express')
+var assert  = require('assert'),
+    express = require('express'),
+    mongodb = require('mongodb')
 
-var app = express();
+var MongoClient = mongodb.MongoClient
+
+var MONGO_URI = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/stackim'
+
+var app = express()
 
 app.enable('trust proxy')
+app.use(express.bodyParser())
 
 
 app.get('/', function (req, resp) {
@@ -12,14 +19,38 @@ app.get('/', function (req, resp) {
 
 app.get('/:tag', function (req, resp) {
     var tag = req.params.tag
-    resp.setHeader('Content-Type', 'text/plain')
-    resp.end('-> http://stackoverflow.com/users/' + tag + '\n')
+    MongoClient.connect(MONGO_URI, function (err, db) {
+        assert.ok(err == null)
+        assert.ok(db != null)
+        
+        db.collection('tags').findOne({'tag': tag}, function (err, rec) {
+            assert.ok(err == null)
+            if (rec == null) {
+                resp.statusCode = 404
+                resp.end("No tag for '" + tag + "'\n")
+            } else {
+                var url = 'http://stackoverflow.com/users/' + rec.stackid
+                resp.redirect(301, url)
+            }
+        })
+    })
 })
 
 app.put('/:tag', function (req, resp) {
-    var tag = req.params.tag
-    resp.setHeader('Content-Tyoe', 'text/plain')
-    resp.end("Creating tag for '" + tag + "'\n")
+    var tag     = req.params.tag,
+        stackid = req.body.stackid
+    MongoClient.connect(MONGO_URI, function (err, db) {
+        assert.ok(err == null)
+        assert.ok(db != null)
+    
+        obj = {'tag': tag, 'stackid': stackid}
+        db.collection('tags').insert(obj, function (err) {
+            assert.ok(err == null)
+            console.log('Created tag: ' + obj)
+            resp.setHeader('Content-Type', 'text/plain')
+            resp.end('OK\n')
+        })
+    })
 })
 
 
