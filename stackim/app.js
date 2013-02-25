2cfa06cbee0342764996360ed20ad58e48ffc4c4
Michael Dippery <michael@monkey-robot.com>
2013-02-25 11:02:01 -0500

    Convert app to use express framework

--- stackim/app.js
+++ stackim/app.js
@@ -1,10 +1,13 @@
-var http = require('http')
+var express = require('express')
+
+var app = express();
+
+app.get('/', function (req, resp) {
+    resp.setHeader('Content-Type', 'text/plain')
+    resp.end('Hello, world!\n')
+})
 
 var port = process.env.PORT || 8000
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello, world!\n')
-}).listen(port, function () {
+app.listen(port, function () {
     console.log('Server running at http://127.0.0.1:' + port + '/')
 })
