ad7f56a30560b9b9ec403d75118030a78e1aab0a
Michael Dippery <michael@monkey-robot.com>
2013-02-27 23:19:41 -0500

    Insert missing "var" statement
    My limited JavaScript knowledge says this isn't strictly necessary
    in this context, but stylishness is next to godliness.

--- stackim/app.js
+++ stackim/app.js
@@ -81,7 +81,7 @@ app.put('/:tag', function (req, resp) {
                     resp.setHeader('Content-Type', 'text/plain')
                     resp.end("Tag '" + tag + "' is already in use\n")
                 } else {
-                    obj = {'tag': tag, 'stackid': stackid, 'createdAt': new Date()}
+                    var obj = {'tag': tag, 'stackid': stackid, 'createdAt': new Date()}
                     db.collection('tags').insert(obj, function (err) {
                         assert.ok(err == null)
                         console.log('Created tag: ' + JSON.stringify(obj))
